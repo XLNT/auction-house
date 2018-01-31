3be4aee69b1aca7f4c70bf907a4d0838710791bd
Feridun Mert Celebi <mertcelebi93@gmail.com>
2018-01-31 13:38:09 -0800

    Change TRUFFLE_HOST => RPC_HOST

--- app/index.js
+++ app/index.js
@@ -6,8 +6,8 @@ import Web3 from "web3";
 if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
 } else {
-  const host = process.env.TRUFFLE_HOST || "127.0.0.1";
-  const port = process.env.TRUFFLE_PORT || 7545;
+  const host = process.env.RPC_HOST || "127.0.0.1";
+  const port = process.env.RPC_PORT || 7545;
   web3 = new Web3(new Web3.providers.HttpProvider(`${host}:${port}`));
 }
 
