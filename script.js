bdacbf3cbef0327c2acfb35ef55dc3849138674d
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2018-02-05 16:33:36 -0800

    Use process.env variables in script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const Web3 = require("web3");
 const contract = require("truffle-contract");
-const repl = require('repl');
+const repl = require("repl");
 
 const AuctionContract = require("./build/contracts/AuctionFactory.json");
 const HillCoreContract = require("./build/contracts/HillCore.json");
 
+const host = process.env.RPC_HOST || "127.0.0.1";
+const port = process.env.RPC_PORT || 7545;
 global.web3 = new Web3(
-  new Web3.providers.HttpProvider("http://localhost:7545")
+  new Web3.providers.HttpProvider(`http://${host}:${port}`)
 );
 
 global.accounts = global.web3.eth.accounts;
@@ -19,18 +22,22 @@ AuctionFactory.setProvider(global.web3.currentProvider);
 const HillCore = contract(HillCoreContract);
 HillCore.setProvider(global.web3.currentProvider);
 
-Promise.all([
-  AuctionFactory.deployed(),
-  HillCore.deployed()
-]).then(([_auctionFactory, _hillCore]) => {
-  global.auctionFactory = _auctionFactory;
-  global.hillCore = _hillCore;
+Promise.all([AuctionFactory.deployed(), HillCore.deployed()])
+  .then(([_auctionFactory, _hillCore]) => {
+    global.auctionFactory = _auctionFactory;
+    global.hillCore = _hillCore;
 
-  global.hillCore.unpause({from: acct0});
-}).then((unpauseResponse) => {
-  global.hillCore.createPromoHill(1, 1, 1, acct1, {from: acct0, gas: 1000000})
-}).then((promoHill) => {
-  repl.start({});
-}).catch((error) => {
-  console.log("ERROR : ", error);
-});
+    global.hillCore.unpause({ from: acct0 });
+  })
+  .then(unpauseResponse => {
+    global.hillCore.createPromoHill(1, 1, 1, acct1, {
+      from: acct0,
+      gas: 1000000
+    });
+  })
+  .then(promoHill => {
+    repl.start({});
+  })
+  .catch(error => {
+    console.log("ERROR : ", error);
+  });
